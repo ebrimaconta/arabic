645c64dae7fc0b76fc13898a9abe5cb62c8ddb39
Ebrima Conta <ebrimaconta@gmail.com>
2021-12-31 22:05:56 +0000

    Added some english words

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
+import { useState } from 'react'
 import arabicEng from '../Data/araibicEng'
 import ReactTooltip from 'react-tooltip'
 export default function Home() {
+  const [toggle, setToggle] = useState(false)
+
   return (
-    <div className="container">
-      {arabicEng.map((word) => {
-        return (
-          <div className="box" key={word.english}>
-            <div data-tip={word.english} className="arabic">
-              {word.arabic}
+    <>
+      <button onClick={() => setToggle(!toggle)}>
+        {toggle ? 'Showing English Words' : 'Remove English Words'}
+      </button>
+      <div className="container">
+        {arabicEng.map((word) => {
+          return (
+            <div className="box" key={word.english}>
+              <div data-tip={toggle ? word.english : ''} className="arabic">
+                {word.arabic}
+              </div>
+              <ReactTooltip place={'bottom'} />
             </div>
-            <ReactTooltip place={'bottom'} />
-          </div>
-        )
-      })}
-    </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
